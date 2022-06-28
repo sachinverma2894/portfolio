51bc13ff341cf3efbdc744bdf28c5e3723437833
Sachin Kumar Verma <sachin.verma@programmers.io>
2022-06-28 17:54:12 +0530

    completed the intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,37 @@
 import './intro.scss';
 
+import TypeWriter from 'typewriter-effect';
+
 const Intro = () => {
   return (
     <div className='intro' id='intro'>
-      Intro
+      <div className='left'>
+        <div className='img-container'>
+          <img src='assets/man.png' alt='' />
+        </div>
+      </div>
+      <div className='right'>
+        <div className='wrapper'>
+          <h2>Hi there, I'm</h2>
+          <h1>Sachin Kumar Verma</h1>
+          <h3>
+            <span>
+              <TypeWriter
+                className='typewriter'
+                options={{
+                  strings: ['Front-end', 'Mainframes'],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </span>{' '}
+            Developer
+          </h3>
+        </div>
+        <a href='#portfolio'>
+          <img src='assets/down.png' alt='' />
+        </a>
+      </div>
     </div>
   );
 };
